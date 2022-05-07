43db4e7398f0784c60ed0334d333d2aaab651297
Warze <40837031+WarzeEgg@users.noreply.github.com>
2022-05-07 13:13:46 +0200

    Initial commit, should have everything

---
+++ main.js
@@ -0,0 +1,172 @@
+const skin_positions = [
+	{
+		"jollymod/characters/billyjoel/Normal_Belly0000.png":
+		[
+			[83, 50],
+			[20, 27, 8, 5, 25, 8, 47, 42],
+			[16, 27, 4, 5, 13, 8, 12, 42]
+		]
+	},
+	{
+		"jollymod/characters/billyjoel/Normal_Head_Idle0000.png":
+		[
+			[96, 96],
+			[8, 8, 8, 8, 21, 19, 67, 67],
+			[0, 8, 8, 8, 8, 19, 13, 67]
+		]
+	},
+	{
+		"jollymod/characters/billyjoel/Normal_Shoulder0000.png":
+		[
+			[41, 70],
+			[44, 20, 4, 12, 14, 0, 21, 70],
+			[40, 20, 4, 12, 6, 0, 8, 70]
+		]
+	},
+	{
+		"jollymod/characters/billyjoel/Normal_Core0000.png":
+		[
+			[121, 113],
+			[20, 20, 8, 10, 41, 3, 50, 110],
+			[16, 20, 4, 10, 29, 3, 12, 110]
+		]
+	},
+	{
+		"jollymod/characters/billyjoel/Normal_Arm0000.png":
+		[
+			[36, 57],
+			[44, 24, 4, 8, 13, 0, 18, 51],
+			[40, 24, 4, 8, 6, 0, 7, 57],
+			[48, 16, 4, 4, 13, 51, 18, 6]
+		]
+	},
+	{
+		"jollymod/characters/billyjoel/Normal_Leg0000.png":
+		[
+			[49, 72],
+			[20, 52, 4, 12, 19, 3, 19, 65],
+			[16, 52, 4, 12, 12, 3, 7, 65]
+		]
+	},
+	{
+		"jollymod/characters/billyjoel/Normal_Thigh0000.png":
+		[
+			[54, 88],
+			[28, 52, 4, 9, 20, 0, 24, 88],
+			[24, 52, 4, 9, 11, 0, 9, 88]
+		]
+	}
+]
+const added_assets = [
+	"jollymod/characters/billyjoel/Mouth_Idle0000.png",
+	"jollymod/characters/billyjoel/Mouth_Pain0000.png",
+	"jollymod/characters/billyjoel/Mouth_Special0000.png",
+	"jollymod/characters/billyjoel/Normal_Eye0000.png",
+	"jollymod/characters/billyjoel/Normal_Eye_Closed0000.png",
+	"jollymod/characters/billyjoel/Normal_Feet0000.png",
+	"jollymod/characters/billyjoel/Normal_Hand0000.png",
+	"jollymod/characters/billyjoel/Normal_Eye0000.png",
+	"jollymod/characters/billyjoel/Normal_Eye0000.png",
+	"jollymod/characters/billyjoel/Normal_Eye0000.png",
+	"jollymod/textures/Textures-Pack-Full0038.png",
+	"jollymod/textures/Textures-Pack-Full0039.png",
+	"jollymod/textures/Textures-Pack-Full0040.png",
+	"jollymod/textures/Textures-Pack-Full0041.png",
+	"jollymod/textures/Textures-Pack-Full0042.png",
+	"jollymod/textures/Textures-Pack-Full0043.png",
+	"jollymod/gore/chunks/billyjoel/Normal_Arm_Gore10000.png",
+	"jollymod/gore/chunks/billyjoel/Normal_Belly_Gore10000.png",
+	"jollymod/gore/chunks/billyjoel/Normal_Core_Gore10000.png",
+	"jollymod/gore/chunks/billyjoel/Normal_Core_Gore20000.png",
+	"jollymod/gore/chunks/billyjoel/Normal_Head_Gore10000.png",
+	"jollymod/gore/chunks/billyjoel/Normal_Head_Gore20000.png",
+	"jollymod/gore/chunks/billyjoel/Normal_Leg_Gore10000.png",
+	"jollymod/gore/chunks/billyjoel/Normal_Shoulder_Gore10000.png",
+	"jollymod/gore/chunks/billyjoel/Normal_Thigh_Gore10000.png",
+	"jollymod/gore/organs/Gore_Brain0000.png",
+	"jollymod/gore/organs/Gore_Intestine0000.png",
+	"jollymod/gore/organs/Gore_Liver0000.png",
+	"jollymod/gore/organs/Gore_LungLeft0000.png",
+	"jollymod/gore/organs/Gore_LungRight0000.png",
+	"jollymod/gore/organs/Gore_Meat10000.png",
+	"jollymod/gore/organs/Gore_Meat20000.png",
+	"jollymod/gore/organs/Gore_Meat30000.png",
+	"jollymod/gore/organs/Gore_Meat40000.png",
+	"jollymod/gore/organs/Gore_Meat50000.png",
+	"jollymod/gore/organs/Gore_Meat60000.png",
+	"jollymod/gore/organs/Gore_Stomach0000.png",
+	"jollymod/gore/skin/adult/Normal_Arm_Flesh0000.png",
+	"jollymod/gore/skin/adult/Normal_Belly_Flesh0000.png",
+	"jollymod/gore/skin/adult/Normal_Core_Flesh0000.png",
+	"jollymod/gore/skin/adult/Normal_Feet_Flesh0000.png",
+	"jollymod/gore/skin/adult/Normal_Hand_Flesh0000.png",
+	"jollymod/gore/skin/adult/Normal_Leg_Flesh0000.png",
+	"jollymod/gore/skin/adult/Normal_Shoulder_Flesh0000.png",
+	"jollymod/gore/skin/adult/Normal_Thigh_Flesh0000.png",
+	"jollymod/vehicles/foddycan/Hammer0000.png",
+	"jollymod/vehicles/foddycan/Hammer_20000.png",
+	"jollymod/vehicles/foddycan/Pot0000.png",
+	"jollymod/vehicles/foddycan/Pot_20000.png",
+	"jollymod/vehicles/skateboard/SkateHelmet0000.png",
+	"jollymod/vehicles/dirtbike/DirtBikeHelmet0000.png",
+]
+function handleFileSelect(input) {
+	const reader = new FileReader()
+	reader.readAsDataURL(input.files[0]);
+	reader.onload = function() {
+		drawSkin(reader.result)
+	}
+}
+
+let zip;
+let imgcount;
+function drawSkin(img) {
+	if (img.width !== 64 || img.height !== 64) {
+		alert("Invalid skin file. Must be 64x64.")
+		return;
+	}
+	imgcount = 0;
+	zip = new JSZip();
+	skin_positions.forEach((file) => {
+		for (const [name, task] of Object.entries(file)) {
+			const cvs = document.createElement('canvas');
+			const ctx = cvs.getContext('2d');
+			cvs.width = task[0][0];
+			cvs.height= task[0][1];
+			ctx.imageSmoothingEnabled = false;
+			for (var i = 1; i < task.length; i ++) {
+				t = task[i]
+				ctx.drawImage(img, t[0], t[1], t[2], t[3], t[4], t[5], t[6], t[7])
+			}
+			cvs.toBlob(function (blob) {
+				zip.file(name, blob)
+				addCount()
+			})
+		}
+	})
+	added_assets.forEach((file) => {
+		const img = new Image();
+		const cvs = document.createElement('canvas');
+		const ctx = cvs.getContext('2d');
+		img.src = file;
+		img.onload = function() {
+			cvs.width = img.width;
+			cvs.height=img.height;
+			ctx.drawImage(img, 0, 0)
+			cvs.toBlob(function (blob) {
+				zip.file(file, blob)
+				addCount();
+			})
+		}
+	})
+}
+
+function addCount() {
+	imgcount ++;
+	if (imgcount === 58) {
+		zip.generateAsync({type:"blob"})
+		.then(function(content) {
+			saveAs(content, "jollymodmcskin" + Math.round(Math.random() * 100000) + ".zip");
+		});
+	}
+}
